dd3ad90599f25003a35167b7657ea149313e3f8a
salamint <salamint@proton.me>
2025-08-10 15:08:14 +0200

    Initial commit

---
+++ BlasII.BetterWeaponSwap/BetterWeaponSwap.cs
@@ -0,0 +1,13 @@
+﻿using BlasII.ModdingAPI;
+
+namespace BlasII.BetterWeaponSwap;
+
+public class BetterWeaponSwap : BlasIIMod
+{
+    internal BetterWeaponSwap() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
+
+    protected override void OnInitialize()
+    {
+        // Perform initialization here
+    }
+}
---
+++ BlasII.BetterWeaponSwap/Main.cs
@@ -0,0 +1,13 @@
+﻿using MelonLoader;
+
+namespace BlasII.BetterWeaponSwap;
+
+internal class Main : MelonMod
+{
+    public static BetterWeaponSwap BetterWeaponSwap { get; private set; }
+
+    public override void OnLateInitializeMelon()
+    {
+        BetterWeaponSwap = new BetterWeaponSwap();
+    }
+}
