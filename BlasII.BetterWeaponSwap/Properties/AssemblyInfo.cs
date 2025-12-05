7f2a1f99f205a002d9de41ea1d0a72fe7c6d78da
salamint <salamint@proton.me>
2025-12-05 12:58:44 +0100

    core: Updated dependencies

--- BlasII.BetterWeaponSwap/Properties/AssemblyInfo.cs
+++ BlasII.BetterWeaponSwap/Properties/AssemblyInfo.cs
@@ -2,5 +2,4 @@ using BlasII.BetterWeaponSwap;
 using MelonLoader;
 
 [assembly: MelonInfo(typeof(BlasII.BetterWeaponSwap.Main), ModInfo.MOD_NAME, ModInfo.MOD_VERSION, ModInfo.MOD_AUTHOR)]
-[assembly: MelonOptionalDependencies("Modding API", "1.1.0")]
-[assembly: MelonOptionalDependencies("Weapons API", "0.1.0")]
+[assembly: MelonOptionalDependencies("Modding API", "0.1.0", "Weapon Events Framework", "0.1.0")]
