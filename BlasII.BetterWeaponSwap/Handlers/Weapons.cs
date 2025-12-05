f8bdf79829bdd319986855093359b42e0188a975
salamint <salamint@proton.me>
2025-12-05 12:41:45 +0100

    weapon: Documented the event handlers

--- BlasII.BetterWeaponSwap/Handlers/Weapons.cs
+++ BlasII.BetterWeaponSwap/Handlers/Weapons.cs
@@ -3,10 +3,14 @@ using BlasII.Framework.WeaponEvents.Events;
 namespace BlasII.BetterWeaponSwap;
 
 /// <summary>
+/// The handleer's role is to reset every saved weapon state when the player
+/// rests at a Prie Dieu.
 /// </summary>
 public class WeaponReseter : WeaponHandler
 {
 	/// <summary>
+	/// When the player rests at a Prie Dieu, this methods calls the Reset
+	/// method of every handler.
 	/// </summary>
 	public override void OnRestAtPrieDieu()
 	{
