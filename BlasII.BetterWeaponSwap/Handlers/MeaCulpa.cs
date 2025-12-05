6f5fabf9568e080c1bc527ba6f294aeb89adcc3e
salamint <salamint@proton.me>
2025-12-05 12:31:47 +0100

    mea culpa: Documented the event handlers

--- BlasII.BetterWeaponSwap/Handlers/MeaCulpa.cs
+++ BlasII.BetterWeaponSwap/Handlers/MeaCulpa.cs
@@ -3,14 +3,20 @@ using BlasII.Framework.WeaponEvents.Events;
 namespace BlasII.BetterWeaponSwap;
 
 /// <summary>
+/// Handles events from the Mea Culpa.
+/// This handler's role is to save the Mea Culpa's berserk mode value
+/// after swapping to another weapon.
 /// </summary>
 public class MeaCulpaBerserkModeSaver : MeaCulpaHandler
 {
 	/// <summary>
+	/// Attribute that saves the current berserk mode value to be restored
+	/// when switching back to Mea Culpa.
 	/// </summary>
 	private static int BerserkModeValueSaved = 0;
 
 	/// <summary>
+	/// Resets the berserk mode value to 0.
 	/// </summary>
 	public static void Reset()
 	{
@@ -18,6 +24,8 @@ public class MeaCulpaBerserkModeSaver : MeaCulpaHandler
 	}
 
     /// <summary>
+	/// Saves the current berserk mode value when swapping from Mea Culpa to
+	/// another weapon.
     /// </summary>
 	public override void OnEquip()
 	{
@@ -25,6 +33,8 @@ public class MeaCulpaBerserkModeSaver : MeaCulpaHandler
 	}
 
     /// <summary>
+	/// Restores the saved berserk mode value when swapping from another weapon
+	/// back to Mea Culpa.
     /// </summary>
 	public override void OnUnequip()
 	{
