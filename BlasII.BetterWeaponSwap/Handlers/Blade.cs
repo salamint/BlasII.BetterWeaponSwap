2d2cd59db2815881e50e22bba4ac9de02b5c128f
salamint <salamint@proton.me>
2025-12-05 12:33:27 +0100

    blade: Documented the event handlers

--- BlasII.BetterWeaponSwap/Handlers/Blade.cs
+++ BlasII.BetterWeaponSwap/Handlers/Blade.cs
@@ -3,14 +3,20 @@ using BlasII.Framework.WeaponEvents.Events;
 namespace BlasII.BetterWeaponSwap;
 
 /// <summary>
+/// Handles events from Ruego al Alba.
+/// This handler's role is to save Ruego al Alba's berserk mode value
+/// after swapping to another weapon.
 /// </summary>
 public class BladeBerserkModeSaver : BladeHandler
 {
 	/// <summary>
+	/// Attribute that saves the current berserk mode value to be restored
+	/// when switching back to Ruego al Alba.
 	/// </summary>
 	private static int BerserkModeValueSaved = 0;
 
 	/// <summary>
+	/// Resets the berserk mode value to 0.
 	/// </summary>
 	public static void Reset()
 	{
@@ -18,6 +24,8 @@ public class BladeBerserkModeSaver : BladeHandler
 	}
 
     /// <summary>
+	/// Saves the current berserk mode value when swapping from Ruego al Alba to
+	/// another weapon.
     /// </summary>
 	public override void OnEquip()
 	{
@@ -25,6 +33,8 @@ public class BladeBerserkModeSaver : BladeHandler
 	}
 
     /// <summary>
+	/// Restores the saved berserk mode value when swapping from another weapon
+	/// back to Ruego al Alba.
     /// </summary>
 	public override void OnUnequip()
 	{
