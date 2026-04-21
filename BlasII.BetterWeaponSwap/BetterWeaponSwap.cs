9d75c0689300ffeb2fd8c846ace47ca6b2b45b8a
salamint <salamint@proton.me>
2026-04-21 19:28:02 +0200

    core: Removed the deprecated ApplyBonus method that has been replaced

--- BlasII.BetterWeaponSwap/BetterWeaponSwap.cs
+++ BlasII.BetterWeaponSwap/BetterWeaponSwap.cs
@@ -1,6 +1,4 @@
 ﻿using BlasII.ModdingAPI;
-using Il2CppTGK.Game.Components.Attack;
-using Il2CppTGK.Game.Components.Attack.Data;
 
 namespace BlasII.BetterWeaponSwap;
 
@@ -11,41 +9,4 @@ public class BetterWeaponSwap : BlasIIMod
 	/// <summary>
 	/// </summary>
     internal BetterWeaponSwap() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
-
-	/// <summary>
-	/// </summary>
-	protected internal void ApplyBonus(RapierTrueSkillFiller filler, AttackInfo attack)
-	{
-		int gain = 0;
-		foreach (var bonus in filler.trueSkillAttackTable.trueSkillBonusTable)
-		{
-			if (bonus.AttackID.id == attack.attackID.id)
-			{
-				gain = bonus.Bonus;
-			}
-		}
-		var stats = filler.playerStats.GetPtr();
-		float y = stats.ApplyBonusAndUpgradesToValue(filler.tsGainStatID, gain);
-
-		int currentValue = stats.GetCurrentValue(filler.trueSkillStatID, false);
-		int maxValue = stats.GetMaxValue(filler.trueSkillStatID);
-		gain = currentValue + (int)y;
-		if (gain < 0)
-		{
-			gain = 0;
-		}
-		else if (maxValue < gain)
-		{
-			gain = maxValue;
-		}
-		stats.SetCurrentValue(filler.trueSkillStatID, gain);
-		filler.consumptionCooldown = filler.consumptionLapseInSeconds;
-	}
-
-	/// <summary>
-	/// </summary>
-    protected override void OnInitialize()
-    {
-        // Perform initialization here
-    }
 }
